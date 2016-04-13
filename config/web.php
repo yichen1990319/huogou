3097318b2858073672e918176bb5f89ce87401c6
chenyi <yichen1990319@163.com>
2016-04-13 18:48:04 +0800

    init

--- config/web.php
+++ config/web.php
@@ -7,9 +7,17 @@ $config = [
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
     'components' => [
+        'urlManager' => [
+            'enablePrettyUrl' => true,
+            'showScriptName' => false,
+            // 是否开启严格解析
+            //'enableStrictParsing' => true,
+            'rules' => [
+            ],
+        ],
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => '',
+            'cookieValidationKey' => 'myhuogou',
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
@@ -38,14 +46,6 @@ $config = [
             ],
         ],
         'db' => require(__DIR__ . '/db.php'),
-        /*
-        'urlManager' => [
-            'enablePrettyUrl' => true,
-            'showScriptName' => false,
-            'rules' => [
-            ],
-        ],
-        */
     ],
     'params' => $params,
 ];
